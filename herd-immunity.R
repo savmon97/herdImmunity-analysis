a5db72f0270db8bc3587549b00880a9e1bbe2d23
savmon97 <57931100+savmon97@users.noreply.github.com>
2021-04-19 12:47:35 -0400

    added two files
    added app file and added analysis file

---
+++ herd-immunity.R
@@ -0,0 +1,12 @@
+#----------------------------------------
+# U.S. Herd-immunity Prediction by County
+# By: Salomon Villatoro
+# Date 4/26/2021
+# GAFL 531
+#----------------------------------------
+
+# Packages Necessary
+library(tidyverse)
+library(stargazer)
+library(rgdal)
+library(leaflet)
